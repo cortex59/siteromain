61056c6731bdf72d96390aad54c31ea2e78e4b29
Rin HARISON <rinharis@gmail.com>
2021-08-07 17:28:10 +0200

    Refactoring menu fonction redirect

--- menu/menu.js
+++ menu/menu.js
@@ -1,16 +1,4 @@
-// Fonctions permettant de sortir de l'iFrame menu pour rediriger la page parent vers les liens
-function GoPortfolioAnimaliere(){
-    top.location = "../portfolio-animaliere.html"
-}
-function GoPortfolioPaysage(){
-    top.location = "../portfolio-paysage.html"
-}
-function GoPortfolioSiteResto(){
-    top.location = "../portfolio-site-restaurant.html"
-}
-function GoProfil(){
-    top.location = "https://www.linkedin.com/in/romain-six"
-}
-function GoCoordonnees(){
-    top.location = "../contact-frame.html"
+// Fonctions permettant de sortir de l'iFrame menu pour rediriger la page parent vers le lien passé en paramètre
+function Redirect(link){
+    top.location = link
 }
