095154014598f55eb6328cabcbe97db508d71b03
Rin HARISON <rinharis@gmail.com>
2021-08-07 04:35:08 +0200

    ajout menu et bouton retour

---
+++ menu/menu.js
@@ -0,0 +1,16 @@
+// Fonctions permettant de sortir de l'iFrame menu pour rediriger la page parent vers les liens
+function GoPortfolioAnimaliere(){
+    top.location = "../portfolio-animaliere.html"
+}
+function GoPortfolioPaysage(){
+    top.location = "../portfolio-paysage.html"
+}
+function GoPortfolioSiteResto(){
+    top.location = "../portfolio-site-restaurant.html"
+}
+function GoProfil(){
+    top.location = "https://www.linkedin.com/in/romain-six"
+}
+function GoCoordonnees(){
+    top.location = "../contact-frame.html"
+}
